98342360903a8c41dcb2328eded89b15245d2b63
Anshuman Tiwari <lazycodebaker@outlook.com>
2024-07-09 20:14:52 +0530

    build

--- server/src/routes/test.ts
+++ server/src/routes/test.ts
@@ -1,13 +1,10 @@
 import { handleUser } from "@/auth/manager";
-import { hashGenerate, verifyPassword } from "@/helper/hash"; 
+import { hashGenerate, verifyPassword } from "@/helper/hash";
 import { Hono } from "hono";
 
 const testRouter = new Hono();
 
-testRouter.get("/", handleUser , async (c) => {
-    const user = c.var.user;
-
-    if(!user) return c.json({ ok: false, message: "Un-Authorised" });
+testRouter.get("/", async (c) => {
     return c.json({ ok: true, message: "hello world from test" });
 });
 
