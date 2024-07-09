1a97cb7beb1e2e862c71fa287b1abc18f9832488
Anshuman Tiwari <lazycodebaker@outlook.com>
2024-07-10 01:19:31 +0530

    build

--- client/src/routes/about.lazy.tsx
+++ client/src/routes/about.lazy.tsx
@@ -1,10 +1,6 @@
 
-import { api } from '@/hono/client';
-import { useQuery } from '@tanstack/react-query';
-import { createLazyFileRoute } from '@tanstack/react-router'
-import { useNavigate } from '@tanstack/react-router'
-import { useEffect } from 'react';
-import { getAuth } from './index.lazy';
+import { api } from '@/hono/client'; 
+import { createLazyFileRoute } from '@tanstack/react-router' 
 
 export const Route = createLazyFileRoute('/about')({
     errorComponent: () => <div>Error While Rendering About Page</div>,
@@ -12,23 +8,6 @@ export const Route = createLazyFileRoute('/about')({
 });
 
 function Index() {
-
-    const { isPending, isLoading, data, error } = useQuery({
-        queryKey: ["auth"],
-        queryFn: getAuth,
-    });
-
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const local = localStorage.getItem("user");
-
-        if (!local) {
-            navigate({
-                to: "/login"
-            });
-        }
-    }, [data, navigate]);
 
     const login = async () => {
         const response = await api.auth.login.$post({
@@ -38,7 +17,7 @@ function Index() {
             }
         })
 
-        console.log(response.json());
+        console.log(await response.json());
 
     };
 
